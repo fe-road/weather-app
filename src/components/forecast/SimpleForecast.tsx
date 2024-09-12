05f8adc0c2fcbad9e895a5e114f6c7eeccf75fc8
Phellipe Perin <phellipe.perin@gmail.com>
2024-09-12 22:11:16 +0200

    part #2 small improvements

--- src/components/forecast/SimpleForecast.tsx
+++ src/components/forecast/SimpleForecast.tsx
@@ -1,24 +1,18 @@
 import { ForecastModel } from '../../models/Forecast';
+import { getFormattedDate } from '../../services/dateService';
 
 interface Props {
     forecast: ForecastModel;
 }
 
 const SimpleForecast = ({ forecast }: Props) => {
-    let currentDay: string | number = forecast.date.getDate();
-    if (currentDay < 10) {
-        currentDay = `0${currentDay}`;
-    }
-    let currentMonth: string | number = forecast.date.getMonth() + 1;
-    if (currentMonth < 10) {
-        currentMonth = `0${currentMonth}`;
-    }
+    const formattedDate = getFormattedDate(forecast.date);
 
     return (
         <article className='flex flex-col px-2 py-3 text-center'>
             <span className='fa-solid fa-sun text-lg mb-2' />
             <p className='mb-0'>{forecast.minTemperature}º - {forecast.maxTemperature}º</p>
-            <span className='text-slate-500 text-xs'>{currentDay}/{currentMonth}</span>
+            <span className='text-slate-500 text-xs'>{formattedDate}</span>
         </article>
     );
 };
--- src/components/weather/WeatherCityList.tsx
+++ src/components/weather/WeatherCityList.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WeatherCityItem from './WeatherCityItem';
 import SearchAddCity from '../form/SearchAddCity';
 
 import { CityModel } from '../../models/City';
+import { MAX_ITEMS } from '../../constants/weather-constants';
 import { fetchCityList, saveCityList } from '../../services/storageService';
-
-const MAX_ITEMS = 5;
 
 const WeatherCityList = () => {
     const [list, setList] = useState<Array<CityModel>>(fetchCityList());
 
     const addCity = (city: CityModel): void => {
         if (list.length < MAX_ITEMS) {
-            setList((currentValue) => {
-                const newItems = [...currentValue, city];
-                saveCityList(newItems);
-                return newItems;
-            });
+            setList((currentValue) => [...currentValue, city]);
         }
     };
 
     const deleteCity = (index: number): void => {
         if (index >= 0 && index < list.length) {
-            setList((currentValue) => {
-                const newItems = currentValue.filter((item, i) => index !== i);
-                saveCityList(newItems);
-                return newItems;
-            });
+            setList((currentValue) => currentValue.filter((_, i) => index !== i));
         }
     };
+
+    useEffect(() => {
+        saveCityList(list);
+    }, [list])
 
     return (
         <section className='flex flex-col gap-y-4 mt-2 text-gray-800'>
